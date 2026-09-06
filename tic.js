21a539eb73bd92cf7ed80d36611ca3d72548bb99
dee8365 <deepronawn1@gmail.com>
2026-09-06 11:24:01 +0530

    Double check press

--- tic.js
+++ tic.js
@@ -13,7 +13,7 @@ let cord1=0;
 let cord2=0;
 const winset=[[0,1,2],[0,3,6],[0,4,8],[2,4,6],[3,4,5],[6,7,8],[1,4,7],[2,5,8]];
 document.querySelector('.pos0').addEventListener('click',() =>{
-    if(document.querySelector('.pos0').textContent != ''){
+    if(document.querySelector('.pos0').textContent == ''){
         ind=0;
         document.querySelector(`.pos${ind}`).textContent="X";
         playing(ind);
@@ -22,7 +22,7 @@ document.querySelector('.pos0').addEventListener('click',() =>{
     }
 });
 document.querySelector('.pos1').addEventListener('click',() =>{
-    if(document.querySelector('.pos1').textContent != ''){
+    if(document.querySelector('.pos1').textContent == ''){
         ind=1;
         document.querySelector(`.pos${ind}`).textContent="X";
         playing(ind);
